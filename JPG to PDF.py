5769fcbd1d17e51c977e18b92efa1ef7f4458fcb
Praneet Kumar <praneetk2704@gmail.com>
2018-10-09 11:00:12 +0530

    Add files via upload

---
+++ JPG to PDF.py
@@ -0,0 +1,45 @@
+from fpdf import FPDF
+from PIL import Image
+import os
+
+pdf = FPDF()
+
+imagelist = []                                                 # Contains the list of all images to be converted to PDF.
+folder = 'C:\\Users\\Praneet Kumar\\Desktop\\New Folder\\'     # Folder containing all the images.
+name = "AI.pdf"                                                # Name of the output PDF file.
+
+
+# ------------- ADD ALL THE IMAGES IN A LIST ------------- #
+
+for dirpath, dirnames, filenames in os.walk(folder):
+    for filename in [f for f in filenames if f.endswith(".jpg")]:
+        full_path = os.path.join(dirpath, filename)
+        imagelist.append(full_path)
+
+imagelist.sort()                                               # Sort the images by name.
+for i in range(0, len(imagelist)):
+    print(imagelist[i])
+
+# --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
+
+for i in range(0, len(imagelist)):
+    im1 = Image.open(imagelist[i])                             # Open the image.
+    width, height = im1.size                                   # Get the width and height of that image.
+    if width > height:
+        im2 = im1.transpose(Image.ROTATE_270)                  # If width > height, rotate the image.
+        os.remove(imagelist[i])                                # Delete the previous image.
+        im2.save(imagelist[i])                                 # Save the rotated image.
+        # im.save
+
+print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
+
+
+# -------------- CONVERT TO PDF ------------ #
+
+for image in imagelist:
+    pdf.add_page()
+    pdf.image(image, 0, 0, 210, 297)                           # 210 and 297 are the dimensions of an A4 size sheet.
+
+pdf.output(folder + name, "F")                                 # Save the PDF.
+
+print("PDF generated successfully!")
