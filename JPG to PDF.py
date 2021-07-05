d4ae2b8f94cc6affc5eb1eec8d645636332ac0da
Apocryphon-X <Apocryphon-X@users.noreply.github.com>
2021-07-05 02:12:50 +0000

    Fix code style issues with Black

--- JPG to PDF.py
+++ JPG to PDF.py
@@ -3,13 +3,13 @@ from PIL import Image
 import os
 
 pdf = FPDF()
-imagelist = []                                                 # Contains the list of all images to be converted to PDF.
+imagelist = []  # Contains the list of all images to be converted to PDF.
 
 
 # --------------- USER INPUT -------------------- #
 
-folder = ""                                                    # Folder containing all the images.
-name = ""                                                      # Name of the output PDF file.
+folder = ""  # Folder containing all the images.
+name = ""  # Name of the output PDF file.
 
 
 # ------------- ADD ALL THE IMAGES IN A LIST ------------- #
@@ -19,19 +19,19 @@ for dirpath, dirnames, filenames in os.walk(folder):
         full_path = os.path.join(dirpath, filename)
         imagelist.append(full_path)
 
-imagelist.sort()                                               # Sort the images by name.
+imagelist.sort()  # Sort the images by name.
 for i in range(0, len(imagelist)):
     print(imagelist[i])
 
 # --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
 
 for i in range(0, len(imagelist)):
-    im1 = Image.open(imagelist[i])                             # Open the image.
-    width, height = im1.size                                   # Get the width and height of that image.
+    im1 = Image.open(imagelist[i])  # Open the image.
+    width, height = im1.size  # Get the width and height of that image.
     if width > height:
-        im2 = im1.transpose(Image.ROTATE_270)                  # If width > height, rotate the image.
-        os.remove(imagelist[i])                                # Delete the previous image.
-        im2.save(imagelist[i])                                 # Save the rotated image.
+        im2 = im1.transpose(Image.ROTATE_270)  # If width > height, rotate the image.
+        os.remove(imagelist[i])  # Delete the previous image.
+        im2.save(imagelist[i])  # Save the rotated image.
         # im.save
 
 print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
@@ -41,8 +41,10 @@ print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n"
 
 for image in imagelist:
     pdf.add_page()
-    pdf.image(image, 0, 0, 210, 297)                           # 210 and 297 are the dimensions of an A4 size sheet.
+    pdf.image(
+        image, 0, 0, 210, 297
+    )  # 210 and 297 are the dimensions of an A4 size sheet.
 
-pdf.output(folder + name, "F")                                 # Save the PDF.
+pdf.output(folder + name, "F")  # Save the PDF.
 
 print("PDF generated successfully!")
