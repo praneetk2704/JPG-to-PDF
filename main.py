77c45c68d521e8cd832b17b848dba6b6b84746b4
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2021-07-05 10:50:46 -0500

    Function refactor

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
 from fpdf import FPDF
 from PIL import Image
+import click
+
+from pathlib import Path
 import os
 
-pdf = FPDF()
-imagelist = []  # Contains the list of all images to be converted to PDF.
+def get_images(directory):
+    """Return a list with all the images in <directory>."""
+    imagelist = []  # Contains the list of all images to be converted to PDF.
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in [f for f in filenames if f.endswith(".jpg")]:
+            full_path = str(Path.joinpath(dirpath, filename))
+            imagelist.append(full_path)
 
+    imagelist.sort()  # Sort the images by name.
+    return imagelist
 
-# --------------- USER INPUT -------------------- #
+def fix_landscape(imglist)
+    """Rotate any landscape mode image if present."""
+    for i in range(0, len(imglist)):
+        im1 = Image.open(imglist[i])  # Open the image.
+        width, height = im1.size  # Get the width and height of that image.
+        if width > height:
+            im2 = im1.transpose(Image.ROTATE_270)  # If width > height, rotate the image.
+            os.remove(imglist[i])  # Delete the previous image.
+            im2.save(imglist[i])  # Save the rotated image.
 
-folder = ""  # Folder containing all the images.
-name = ""  # Name of the output PDF file.
+def make_pdf(directory, name, imglist)
+    """Convert <imglist> into a PDF and saves it in <directory> as <name>."""
+    pdf = FPDF()
+    for image in imglist:
+        pdf.add_page()
+        pdf.image(
+            image, 0, 0, 210, 297
+        )  # 210 and 297 are the dimensions of an A4 size sheet.
 
+    pdf.output(directory + name, "F")  # Save the PDF.
 
-# ------------- ADD ALL THE IMAGES IN A LIST ------------- #
-
-for dirpath, dirnames, filenames in os.walk(folder):
-    for filename in [f for f in filenames if f.endswith(".jpg")]:
-        full_path = os.path.join(dirpath, filename)
-        imagelist.append(full_path)
-
-imagelist.sort()  # Sort the images by name.
-for i in range(0, len(imagelist)):
-    print(imagelist[i])
-
-# --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
-
-for i in range(0, len(imagelist)):
-    im1 = Image.open(imagelist[i])  # Open the image.
-    width, height = im1.size  # Get the width and height of that image.
-    if width > height:
-        im2 = im1.transpose(Image.ROTATE_270)  # If width > height, rotate the image.
-        os.remove(imagelist[i])  # Delete the previous image.
-        im2.save(imagelist[i])  # Save the rotated image.
-        # im.save
-
-print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
-
-
-# -------------- CONVERT TO PDF ------------ #
-
-for image in imagelist:
-    pdf.add_page()
-    pdf.image(
-        image, 0, 0, 210, 297
-    )  # 210 and 297 are the dimensions of an A4 size sheet.
-
-pdf.output(folder + name, "F")  # Save the PDF.
-
-print("PDF generated successfully!")
